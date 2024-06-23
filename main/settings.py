2f17aa12d0eec7ad523d766b6dfd00274adbdff1
Edouard102 Trentesaux <edouard102@gmx.fr>
2024-06-23 11:12:27 +0000

    fix html error in contact.html

--- main/settings.py
+++ main/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-edouard102-maerestauran-75d85ha3it9.ws-eu110.gitpod.io', 'mae-restaurant-fd1d9cf11a78.herokuapp.com', '.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['8000-edouard102-maerestauran-d5xqkmciq3j.ws-eu114.gitpod.io', 'mae-restaurant-fd1d9cf11a78.herokuapp.com', '.herokuapp.com','localhost']
 
 # added from https://stackoverflow.com/questions/77208322/forbidden-origin-checking-failed-https-api-example-com-does-not-match-any-t 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
